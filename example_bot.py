f7e92c44acf2b17e3783c4c0d23e00b974b14c0a
AndrewLive <andrewvinhtonthat@gmail.com>
2022-05-01 22:12:36 -0700

    added deez_nuts implementation

--- example_bot.py
+++ example_bot.py
@@ -17,8 +17,14 @@ class MyClient(discord.Client):
         if message.author == client.user:
             return
 
-        print('Message from {0.author}: {0.content}'.format(message))
-        await message.channel.send('Message from {0.author}: {0.content}'.format(message))
+        ## print('Message from {0.author}: {0.content}'.format(message))
+        ## await message.channel.send('Message from {0.author}: {0.content}'.format(message))
+        await self.deez_nuts_check(message)
+
+    async def deez_nuts_check(self, message):
+        for substring in deez_nuts_dict:
+            if substring in message.content.lower().strip():
+                await message.reply(f'> *{substring}*\n{deez_nuts_dict[substring]}', mention_author=False)
 
 
 client = MyClient()
@@ -32,4 +38,17 @@ print(TOKEN)
 
 config_file.close()
 
+
+# Read deez_nutz file to get dictionary of deez nutz replies
+deez_nuts_file = open("discord_bot\deez_nuts.txt", "r")
+
+deez_nuts_dict = {}
+for i in deez_nuts_file.readlines():
+    i = i.strip().split(": ")
+    deez_nuts_dict[i[0]] = i[1]
+
+deez_nuts_file.close()
+
+
+# Run the discord client
 client.run(TOKEN)
