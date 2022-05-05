c62dd0c23e1e3c7a883351136c912618b229087f
AndrewLive <andrewvinhtonthat@gmail.com>
2022-05-05 03:07:43 -0700

    added translate command also restructured code to use commands.bot instead of discord.client

--- lea_bot.py
+++ lea_bot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 import logging
 from googletrans import Translator
+from googletrans import LANGUAGES
 
 
 # configure the logging module
@@ -13,40 +14,71 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+bot = commands.Bot(command_prefix='!')
 
-class MyClient(discord.Client):
-    # Executes once at the start of the bot's runtime when bot is ready
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+## BOT FUNCTIONS ##
 
-    # Executes whenever a message is sent
-    async def on_message(self, message): 
-        if message.author == bot.user:
-            return
+## Passives
 
-        await self.deez_nuts_check(message)
-        await self.translate_message(message)
-            
-    # Deez Nuts checker
-    # Finishes the deez nuts joke based on the message sent
-    async def deez_nuts_check(self, message):
-        for substring in deez_nuts_dict:
-            if substring in message.content.lower().strip():
-                await message.reply(f'> *{substring}*\n{deez_nuts_dict[substring]}', mention_author=False)
+# Executes once at the start of the bot's runtime when bot is ready
+@bot.event
+async def on_ready():
+    print('Logged on as {0}!'.format(bot.user))
+    return
 
-    # English Translator
-    # Automatically translates non-english messages to English
-    # TODO resolve 2000 char limit problem (possibly use embeds)
-    async def translate_message(self, message):
-        detectedLang = translator.detect(message.content)
-        if detectedLang.lang != "en" and detectedLang.confidence > 0.2:
-            translated = translator.translate(message.content, "en")
-            await message.reply(f'> *{message.content}*\n`{translated.src} to {translated.dest}: confidence: {detectedLang.confidence}`\n{translated.text}', mention_author=False)
+# Executes whenever a message is sent
+@bot.event
+async def on_message(message): 
+    if message.author == bot.user:
+        return
 
-    # TODO commands for translate and help and an admin kill
+    await deez_nuts_check(message)
+    # await translate_message(message)
+
+    await bot.process_commands(message)
+
+    return
 
 
 # Commands
+# TODO: add translate and help commands
+
+# Translate Command
+# User replies to a message to be translated with the command
+@bot.command()
+async def translate(ctx, dest='en'):
+    if dest not in LANGUAGES:
+        await ctx.reply('Invalid language code', mention_author=True)
+        return
+
+    if ctx.message.reference == None:
+        await ctx.reply('No message to translate', mention_author=True)
+        return
+
+    # print(dest)
+    original_message = await ctx.fetch_message(ctx.message.reference.message_id)
+    await translate_message(original_message, dest)
+    return
+        
+
+
+## FUNCTION IMPLEMENTATIONS ##
+
+# Deez Nuts checker
+# Finishes the deez nuts joke based on the message sent
+async def deez_nuts_check(message):
+    for substring in deez_nuts_dict:
+        if substring in message.content.lower().strip():
+            await message.reply(f'> *{substring}*\n{deez_nuts_dict[substring]}', mention_author=False)
+
+# English Translator
+# Automatically translates non-english messages to English
+# TODO resolve 2000 char limit problem (possibly use embeds)
+async def translate_message(message, dest='en'):
+    detectedLang = translator.detect(message.content)
+    if detectedLang.lang != dest and detectedLang.confidence > 0.2:
+        translated = translator.translate(message.content, dest)
+        await message.reply(f'> *{message.content}*\n`{translated.src} to {translated.dest}: confidence: {detectedLang.confidence}`\n{translated.text}', mention_author=False)
 
 
 
@@ -72,6 +104,5 @@ deez_nuts_file.close()
 translator = Translator()
 
 
-# Run the discord client
-bot = MyClient()
+# Run the discord bot
 bot.run(TOKEN)
