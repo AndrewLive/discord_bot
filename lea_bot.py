8f6981d8a60c79ec18c0d0f0c23343acefed0c9e
AndrewLive <andrewvinhtonthat@gmail.com>
2022-05-16 18:42:11 -0700

    added support for unicode deez nuts jokes

--- lea_bot.py
+++ lea_bot.py
@@ -97,12 +97,14 @@ config_file.close()
 
 
 # Read deez_nutz file to get dictionary of deez nutz replies
-deez_nuts_file = open("discord_bot\deez_nuts.txt", "r")
+deez_nuts_file = open("discord_bot\deez_nuts.txt", "r", encoding = 'utf-8')
 deez_nuts_dict = {}
 for i in deez_nuts_file.readlines():
     i = i.strip().split(": ")
     deez_nuts_dict[i[0]] = i[1]
 deez_nuts_file.close()
+
+print(deez_nuts_dict)
 
 
 # Initialize the Translator object
